425d8b9330582ce0d66f1defa6b27266ddcc700b
Aad Versteden <madnificent@gmail.com>
2025-03-07 00:52:15 +0100

    First results from new basket fetching strategy
    These are the first results from the new basket fetching strategy.  This
    strategy splits up the queries so we have some more obvious control over which
    parts we'd want to export in case multiple information points are available.

--- app.js
+++ app.js
@@ -59,6 +59,87 @@ async function basketsWithStatus( { status, from, to } ) {
   }
 }
 
+/**
+ * Returns the delivery information for the given basket.
+ */
+async function basketDeliveryInfo( basket, graph ) {
+  const selectedDeliveryPlaceAnswer = (await query(`
+    PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+
+    SELECT DISTINCT ?deliveryType ?deliveryPlaceUri ?deliveryPlace ?routeUri ?route WHERE {
+      GRAPH ${sparqlEscapeUri(graph)} {
+        VALUES ?basket { ${sparqlEscapeUri( basket )} }
+        ?basket a veeakker:Basket.
+        OPTIONAL { ?basket veeakker:deliveryType ?deliveryType. }
+        }
+      OPTIONAL {
+        GRAPH ${sparqlEscapeUri(graph)} {
+          ?basket a veeakker:Basket.
+          ?basket veeakker:deliveryPlace ?deliveryPlaceUri.
+        }
+        GRAPH <http://mu.semte.ch/graphs/public> {
+          ?deliveryPlaceUri dct:title ?deliveryPlace.
+          OPTIONAL {
+            ?deliveryPlaceUri
+              veeakker:belongsToRoute ?routeUri;
+              dct:title ?route.
+          }
+        }
+      }
+    }
+  `, { sudo: true })).results.bindings;
+
+  // custom delivery place // TODO: this probably belongs elsewhere
+  const customDeliveryAddressAnswer = (await query(`
+    PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX schema: <http://schema.org/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX gr: <http://purl.org/goodrelations/v1#>
+    PREFIX adms: <http://www.w3.org/ns/adms#>
+
+    SELECT DISTINCT
+      (CONCAT(?firstName, " ", ?lastName) AS ?name)
+      ?deliveryAddress
+      ?phone
+      ?email
+      ?streetAddress
+      ?postalCode
+      ?locality
+      ?user
+      ?companyInfo
+    WHERE {
+      GRAPH ${sparqlEscapeUri(graph)} {
+        VALUES ?basket { ${sparqlEscapeUri( basket )} }
+        ?basket veeakker:hasCustomDeliveryPlace ?hasCustomDeliveryPlace.
+        ?basket veeakker:deliveryAddress ?deliveryAddress.
+        ?deliveryAddress
+          schema:email ?email;
+          foaf:firstName ?firstName;
+          foaf:lastName ?lastName;
+          foaf:phone ?phone.
+        ?deliveryAddress schema:hasAddress ?address.
+
+        ?address
+          schema:addressLocality ?locality;
+          schema:postalCode ?postalCode;
+          schema:streetAddress ?streetAddress.
+
+        OPTIONAL {
+          ?deliveryAddress ext:companyInfo ?companyInfo.
+        }
+      }
+      FILTER (?hasCustomDeliveryPlace)
+    }
+  `, { sudo: true })).results.bindings;
+
+  console.log( { customDeliveryAddressAnswer, selectedDeliveryPlaceAnswer } );
+  return { customDeliveryAddressAnswer, selectedDeliveryPlaceAnswer };
+}
+
 async function basketUserInfo( basket, graph ) {
   // TODO: should we split up the user info from the delivery info?  That might make reading in the data more obvious
   // towards import too because we could register a standard place for order separately from the how the specific order
@@ -157,59 +238,13 @@ async function basketUserInfo( basket, graph ) {
     }
   `, { sudo: true })).results.bindings;
 
-  // 3. custom delivery place // TODO: this probably belongs elsewhere
-  const customDeliveryAddressAnswer = (await query(`
-    PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
-    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX schema: <http://schema.org/>
-    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-    PREFIX gr: <http://purl.org/goodrelations/v1#>
-    PREFIX adms: <http://www.w3.org/ns/adms#>
-
-    SELECT DISTINCT
-      (CONCAT(?firstName, " ", ?lastName) AS ?name)
-      ?deliveryAddress
-      ?phone
-      ?email
-      ?streetAddress
-      ?postalCode
-      ?locality
-      ?user
-      ?companyInfo
-    WHERE {
-      GRAPH ${sparqlEscapeUri(graph)} {
-        VALUES ?basket { ${sparqlEscapeUri( basket )} }
-        ?basket veeakker:hasCustomDeliveryPlace ?hasCustomDeliveryPlace.
-        ?basket veeakker:deliveryAddress ?deliveryAddress.
-        ?deliveryAddress
-          schema:email ?email;
-          foaf:firstName ?firstName;
-          foaf:lastName ?lastName;
-          foaf:phone ?phone.
-        ?deliveryAddress schema:hasAddress ?address.
-
-        ?address
-          schema:addressLocality ?locality;
-          schema:postalCode ?postalCode;
-          schema:streetAddress ?streetAddress.
-
-        OPTIONAL {
-          ?deliveryAddress ext:companyInfo ?companyInfo.
-        }
-      }
-      FILTER (?hasCustomDeliveryPlace)
-    }
-  `, { sudo: true })).results.bindings;
-
-  // 4. combine the results
+  // 3. combine the results
   console.log( {
-    loggedInUserAnswer, invoiceAddressAnswer, customDeliveryAddressAnswer
+    loggedInUserAnswer, invoiceAddressAnswer
   } );
 
   return {
-    loggedInUserAnswer, invoiceAddressAnswer, customDeliveryAddressAnswer
+    loggedInUserAnswer, invoiceAddressAnswer
   }
 }
 
@@ -271,6 +306,24 @@ async function basketOrderLines( basket, graph ) {
   return response.results.bindings;
 }
 
+/**
+  * Joins multiple arrays together by taking their cross product, joining all properties.
+ * @param {Array} records Array of objects to be joined.
+ * @param {Array<Array>} others Other arrays of objects to be joined.
+ * @result {Array} Each combinedresult
+ */
+function joinResults(records, ...others) {
+  if( others.length == 0 ) {
+    return records; // arg is an array and contains the sole results
+  } else {
+    let results = [];
+    for( let flattenedNestedRecord of joinResults( ...others ) )
+      for( let record of records )
+        results.push( Object.assign( {}, record, flattenedNestedRecord ) )
+    return results;
+  }
+}
+
 app.get('/baskets', async function( req, res ) {
   if( await isAdminUser(req) ) {
     try {
@@ -284,30 +337,66 @@ app.get('/baskets', async function( req, res ) {
 
       for ( let basketInfo of basketsResponse.results.bindings ) {
         // each basket
+        let infoToCombine = [];
         // find the user's information
-        let userInfo = basketUserInfo( basketInfo.basket.value, basketInfo.graph.value );
+        let userInfo = await basketUserInfo( basketInfo.basket.value, basketInfo.graph.value );
         // find delivery information
-        let deliveryInfo = basketDeliveryInfo( basketInfo.basket.value, basketInfo.graph.value );
+        let deliveryInfo = await basketDeliveryInfo( basketInfo.basket.value, basketInfo.graph.value );
         // collect orderlines
-        let orderLines = basketOrderLines( basketInfo.basket.value, basketInfo.graph.value );
-        joinResults( basketInfo, userInfo, deliveryInfo, orderLines )
+        let orderLines = await basketOrderLines( basketInfo.basket.value, basketInfo.graph.value );
+
+        if ( userInfo.loggedInUserAnswer.length )
+          infoToCombine.push(userInfo.loggedInUserAnswer)
+        if ( userInfo.loggedInUserAnswer.length > 1 )
+          console.warn("Got more than one user for ${basketInfo.basket.value} ${basketInfo.graph.value}");
+
+        if ( userInfo.invoiceAddressAnswer.length )
+          // last one wins (vs logged in user info or custom delivery address)
+          infoToCombine.push(userInfo.invoiceAddressAnswer)
+        if ( userInfo.invoiceAddressAnswer.length > 1 )
+          console.warn("Got more than one invoice address for ${basketInfo.basket.value} ${basketInfo.graph.value}");
+        
+        if ( deliveryInfo.customDeliveryAddressAnswer.length )
+          infoToCombine.push(deliveryInfo.customDeliveryAddressAnswer)
+        if ( deliveryInfo.customDeliveryAddressAnswer.length > 1 )
+          console.warn("Got more than one delivery info for ${basketInfo.basket.value} ${basketInfo.graph.value}");
+
+        if ( deliveryInfo.selectedDeliveryPlaceAnswer )
+          infoToCombine.push(deliveryInfo.selectedDeliveryPlaceAnswer)
+        if ( deliveryInfo.selectedDeliveryPlaceAnswer.length > 1 )
+          console.warn("Got more than one selected delivery address for ${basketInfo.basket.value} ${basketInfo.graph.value}");
+
+        infoToCombine.push( orderLines );
+        
+        joinResults( ...infoToCombine )
           .forEach( (line) => allOrderLines.push(line) )
       }
 
-      // bindingsAndHeadersAsCsv( allOrderLines,
-      //   [
-      //     "plu",
-      //     "aantalPakjes", "besteldStuks", "besteldGram",
-      //     "basket",
-      //     "name", "address", "phone", "email", "streetAddress", "postalCode", "locality", "user",
-      //     "lastChange",
-      //     "hasCustomDeliveryPlace", "deliveryType", "deliveryPlace",
-      //     "orderLine", "comment", "totalPrice"
-      //   ] );
-
-      //  res
-      //    .status(200)
-      //    .send( queryAnswerAsCsv(response) );
+      res
+        .status(200)
+        .send(bindingsAndHeadersAsCsv( allOrderLines, [
+          "deliveryType", "deliveryPlaceUri", "deliveryPlace", "routeUri", "route",
+          "name",
+          "userAddress",
+          "phone",
+          "email",
+          "streetAddress",
+          "postalCode",
+          "locality",
+          "companyInfo",
+          "address",
+          "user",
+          "deliveryType", "deliveryPlaceUri", "deliveryPlace", "routeUri", "route",
+          "deliveryAddress",
+          "companyInfo",
+          "plu",
+          "aantalPakjes",
+          "totalPrice",
+          "besteldStuks",
+          "besteldGram",
+          "basket",
+          "comment"
+        ]));
     } catch (e) {
     console.log(e);
     }
@@ -319,5 +408,33 @@ app.get('/baskets', async function( req, res ) {
         {errors: [{code: "403", message: "Forbidden, not administrator"}]}))
   }
 } );
+
+const monitoredBaskets = [{
+  basket: "http://veeakker.be/baskets/e8a2fe00-ee13-11ef-8ef3-9bd228564710",
+  graph: "http://mu.semte.ch/sessions/e8591c9a-ee13-11ef-9f59-0242ac170012",
+  label: "Anonymous"
+},{
+  basket: "http://veeakker.be/baskets/241ad4b0-ee2a-11ef-8ef3-9bd228564710",
+  graph: "http://mu.semte.ch/sessions/2407b63c-ee2a-11ef-a0fd-0242ac170012",
+  label: "Anonymous custom deliveryplace"
+},{
+  basket: "http://veeakker.be/baskets/19a2af20-187f-11ef-8b39-4b9115e3448f",
+  graph: "http://veeakker.be/people/146a2ab0-187f-11ef-89e6-db30b83ad484",
+  label: "Logged in"
+}];
+
+console.log( joinResults([{one: 1},{two: 2}, {three: 3}]) )
+console.log( joinResults([{one: 1},{two: 2}, {three: 3}], [{a: "a"}]) )
+console.log( joinResults([{one: 1},{two: 2}, {three: 3}], [{a: "a"}, {b: "b"}]) )
+
+for( let {basket, graph, label} of monitoredBaskets ) {
+  console.log( {label, info: await basketUserInfo(basket, graph)} );
+  console.log( {label, orderLines: await basketOrderLines(basket, graph)} );
+}
+
+console.log( joinResults([{one: 1},{two: 2}, {three: 3}]) )
+console.log( joinResults([{one: 1},{two: 2}, {three: 3}], [{a: "a"}]) )
+console.log( joinResults([{one: 1},{two: 2}, {three: 3}], [{a: "a"}, {b: "b"}]) )
+
 
 app.use(errorHandler);
